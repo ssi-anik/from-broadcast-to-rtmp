dd35fc3c11f5205866083523e417b529b5cbf6f3
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-09-21 23:25:32 +0600

    extract stream URL for YT videos

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,97 @@
 import arg from 'arg';
-import inquirer from "inquirer";
+import {
+    allowedFrom,
+    allowedTo,
+    askForInput,
+    defaultFrom,
+    defaultTo,
+    fancyLogger,
+    getAllowedOrDefault,
+    getYoutubeVideoId,
+    getHlsUrlForYoutubeVideo
+} from "./helpers";
 
-const allowedFrom = {
-    'yt': 'youtube',
-    'youtube': 'youtube',
-};
-const allowedTo = {
-    'fb': 'facebook',
-    'facebook': 'facebook',
-};
-const defaultFrom = 'youtube';
-const defaultTo = 'facebook';
-
-function getAllowedOrDefault (allowed, provided, defaultSelected) {
-    return allowed[provided && provided.toLowerCase() || defaultSelected] || defaultSelected
+function getYoutubeStreamUrl (url) {
+    //YoutubeVideo.json.streamingData:.hlsManifestUrl
+    return scrapper.getVideoInfo(url).then(video => {
+        console.log(video.info.isLiveContent, video.info);
+        return new Promise(resolve => {
+            return resolve(video.YoutubeVideo.json.streamingData.hlsManifestUrl || '')
+        });
+    }).catch(e => {
+        fancyLogger(`Error occurred when parsing video from: "${url}". Reason: ${e.message}`, 'red');
+        process.exit(1);
+    });
 }
+
+async function setHLSUrl (options) {
+    const streamUrl = options.streamURL;
+    let url = '';
+    if ( options.from === allowedFrom['youtube'] ) {
+        url = await getYoutubeStreamUrl(streamUrl);
+    }
+
+    if ( url.length === 0 ) {
+        throw Error('Cannot fetch stream URL for: ' + streamUrl);
+    }
+
+    return {
+        ...options,
+        hls_url: url,
+    };
+}
+
+// ------------
+
+/**
+ * RTMP Server related methods
+ */
+
+function askForRTMPKey (server) {
+    return new Promise((resolve, reject) => {
+        if ( allowedTo['facebook'] === server ) {
+            resolve(askForFacebookRTMPKey());
+        }
+
+        reject(`Invalid RTMP server. ${server}`);
+    });
+}
+
+function askForFacebookRTMPKey () {
+    const inputMessage = 'Enter your RTMP key [Facebook https://www.facebook.com/live/producer]:';
+    const errorMessage = 'RTMP key cannot be empty';
+
+    return askForInput(inputMessage, 1, errorMessage);
+}
+
+/**
+ * Stream broadcaster related methods
+ */
+
+function askForStreamUrl (service) {
+    return new Promise((resolve, reject) => {
+        if ( allowedFrom['youtube'] === service ) {
+            resolve(askForYoutubeURL());
+        }
+
+        reject(`Invalid broadcasting server. ${service}`);
+    });
+}
+
+function askForYoutubeURL () {
+    return askForInput('Enter streaming URL [Youtube]:').then(url => {
+        url = (url || '').trim()
+        if ( !url ) {
+            throw Error('URL cannot be empty.');
+        }
+
+        return getHlsUrlForYoutubeVideo(getYoutubeVideoId(url));
+    });
+}
+
+/**
+ * Parser
+ */
 
 function parseArgToOptions (rawArgs) {
     const args = arg({
@@ -27,49 +104,32 @@ function parseArgToOptions (rawArgs) {
         argv: rawArgs.slice(2),
     });
 
-    return {
-        'from': getAllowedOrDefault(allowedFrom, args['--from'], defaultFrom),
-        'to': getAllowedOrDefault(allowedTo, args['--to'], defaultTo)
-    };
+    return new Promise(resolve => {
+        resolve({
+            'from': getAllowedOrDefault(allowedFrom, args['--from'], defaultFrom),
+            'to': getAllowedOrDefault(allowedTo, args['--to'], defaultTo)
+        })
+    });
 }
-
-function askForFacebookRTMPKey () {
-    return inquirer.prompt({
-        'type': 'input',
-        'name': 'key',
-        'message': 'Enter your RTMP key for Facebook: ',
-    }).then(r => r.key);
-}
-
-function askForYoutubeURL () {
-    return inquirer.prompt({
-        'type': 'input',
-        'name': 'url',
-        'message': 'Enter broadcasting URL from Youtube: ',
-    }).then(r => r.url);
-}
-
-async function askForRequiredData (options) {
-    if ( options.to === allowedTo['facebook'] ) {
-        options = {
-            ...options,
-            rtmpKey: await askForFacebookRTMPKey()
-        }
-    }
-
-    if ( options.from === allowedFrom['youtube'] ) {
-        options = {
-            ...options,
-            streamURL: await askForYoutubeURL()
-        }
-    }
-
-    return options;
-}
-
 
 export async function cli (args) {
-    let options = parseArgToOptions(args);
-    options = await askForRequiredData(options);
-    console.log(options);
+    parseArgToOptions(args)
+        .then(options => {
+            return askForRTMPKey(options.to).then(key => {
+                return {
+                    ...options,
+                    rtmpKey: key
+                };
+            });
+        })
+        .then(options => {
+            return askForStreamUrl(options.from).then(url => {
+                return {
+                    ...options,
+                    hlsUrl: url,
+                };
+            });
+        })
+        .then(options => console.log(options))
+        .catch(e => fancyLogger(`[ERROR]: ${e.message}`, 'red'));
 }
---
+++ src/helpers.js
@@ -0,0 +1,115 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import axios from "axios";
+
+export const allowedFrom = {
+    'yt': 'youtube',
+    'youtube': 'youtube',
+};
+export const allowedTo = {
+    'fb': 'facebook',
+    'facebook': 'facebook',
+};
+export const defaultFrom = 'youtube';
+export const defaultTo = 'facebook';
+
+export function getAllowedOrDefault (allowed, provided, defaultSelected) {
+    return allowed[provided && provided.toLowerCase() || defaultSelected] || defaultSelected
+}
+
+export function logger (text) {
+    console.log(text);
+}
+
+export function getYoutubeVideoId (str) {
+    /**
+     * Sauce: https://github.com/Rubenennj/youtube-scrapper/blob/81371c07128f5601dce53563865c581183d10b7d/src/util/Util.ts#L42
+     */
+    // https://www.youtube.com/v/s2h28p4s-Xs
+    if ( str.includes("/v/") ) {
+        return str.split("/v/")[1].split("&")[0]
+    }
+    // https://www.youtube.com/embed/s2h28p4s-Xs
+    if ( str.includes("youtube.com/embed/") ) {
+        return str.split("embed/")[1].split("&")[0]
+    }
+    // https://youtu.be/s2h28p4s-Xs
+    if ( str.includes("youtu.be/") && !str.includes("/v/") ) {
+        return str.split("youtu.be/")[1].split("&")[0];
+    }
+    // https://www.youtube.com/watch?v=s2h28p4s-Xs
+    if ( str.includes("watch?v=") ) {
+        return str.split("watch?v=")[1].split("&")[0]
+    }
+
+    // everything else
+    return str;
+}
+
+export function objectTraversal (data, keys, defaultValue = undefined) {
+    if ( keys.length === 0 ) {
+        return data;
+    }
+
+    const key = keys.shift();
+    const selectedNodeValue = data[key];
+    if ( selectedNodeValue === undefined ) {
+        return defaultValue;
+    }
+
+    return objectTraversal(selectedNodeValue, keys, defaultValue);
+}
+
+export function getHlsUrlForYoutubeVideo (videoId) {
+    // https://github.com/Rubenennj/youtube-scrapper/blob/81371c07128f5601dce53563865c581183d10b7d/src/functions/getVideoInfo.ts#L5
+    return axios.get(`https://www.youtube.com/watch?v=${videoId}&hl=en`).then(r => {
+        const json = JSON.parse(r.data.split("var ytInitialPlayerResponse = ")[1].split(";</script>")[0]);
+
+        const reason = objectTraversal(json, [
+            'playabilityStatus',
+            'reason'
+        ], false);
+        if ( reason ) {
+            throw Error(reason);
+        }
+
+        const isLiveContent = objectTraversal(json, [
+            'videoDetails',
+            'isLiveContent',
+        ], false);
+        if ( false === isLiveContent ) {
+            throw Error('The provided URL is not streaming live.');
+        }
+
+        const url = objectTraversal(json, [
+            'streamingData',
+            'hlsManifestUrl',
+        ], '');
+
+        if ( url === '' ) {
+            throw Error('Cannot extract data for the URL.')
+        }
+
+        return url;
+    });
+}
+
+export function fancyLogger (text, color = 'green') {
+    logger(chalk.keyword(color)(text));
+}
+
+export function askForInput (message, minimumLength = null, errorMessage = null) {
+    return inquirer.prompt({
+        'type': 'input',
+        'name': 'input',
+        message
+    }).then(r => {
+        if ( (null === minimumLength) || (r.input.length >= minimumLength) ) {
+            return r.input;
+        }
+
+        errorMessage = errorMessage || `Invalid input. Minimum length: ${minimumLength || 'N/A'}`;
+
+        throw Error(errorMessage);
+    });
+}
