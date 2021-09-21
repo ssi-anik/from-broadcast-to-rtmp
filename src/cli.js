5487fa10439064ece8a4838168185cb27f548bd1
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2021-09-21 20:06:32 +0600

    reads for required data from user and pushes to options

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,75 @@
-export function cli (args) {
-    console.log(args);
+import arg from 'arg';
+import inquirer from "inquirer";
+
+const allowedFrom = {
+    'yt': 'youtube',
+    'youtube': 'youtube',
+};
+const allowedTo = {
+    'fb': 'facebook',
+    'facebook': 'facebook',
+};
+const defaultFrom = 'youtube';
+const defaultTo = 'facebook';
+
+function getAllowedOrDefault (allowed, provided, defaultSelected) {
+    return allowed[provided && provided.toLowerCase() || defaultSelected] || defaultSelected
+}
+
+function parseArgToOptions (rawArgs) {
+    const args = arg({
+        '--from': String,
+        '--to': String,
+
+        '-f': '--from',
+        '-t': '--to',
+    }, {
+        argv: rawArgs.slice(2),
+    });
+
+    return {
+        'from': getAllowedOrDefault(allowedFrom, args['--from'], defaultFrom),
+        'to': getAllowedOrDefault(allowedTo, args['--to'], defaultTo)
+    };
+}
+
+function askForFacebookRTMPKey () {
+    return inquirer.prompt({
+        'type': 'input',
+        'name': 'key',
+        'message': 'Enter your RTMP key for Facebook: ',
+    }).then(r => r.key);
+}
+
+function askForYoutubeURL () {
+    return inquirer.prompt({
+        'type': 'input',
+        'name': 'url',
+        'message': 'Enter broadcasting URL from Youtube: ',
+    }).then(r => r.url);
+}
+
+async function askForRequiredData (options) {
+    if ( options.to === allowedTo['facebook'] ) {
+        options = {
+            ...options,
+            rtmpKey: await askForFacebookRTMPKey()
+        }
+    }
+
+    if ( options.from === allowedFrom['youtube'] ) {
+        options = {
+            ...options,
+            streamURL: await askForYoutubeURL()
+        }
+    }
+
+    return options;
+}
+
+
+export async function cli (args) {
+    let options = parseArgToOptions(args);
+    options = await askForRequiredData(options);
+    console.log(options);
 }
